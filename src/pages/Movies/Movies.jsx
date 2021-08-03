d87a7d5c5e68f0185be688e8524f091f1cce541c
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-08-03 22:20:27 +0400

    feat: get all movie info with axios

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Movies() {
-    return (
-        <span className="pageTitle">Top Movies</span>
-    )
+  const [content, setContent] = useState([]);
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  fetchMovies();
+
+  return (
+    <>
+      <span className="pageTitle">Top Movies</span>
+      <div className="movies">
+        {content && content.map((movie) => console.log(movie))}
+      </div>
+    </>
+  );
 }
