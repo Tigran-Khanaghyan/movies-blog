98f406ea3a295b60d647aa04dc1064dd9b94ede2
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-08-07 20:26:38 +0400

    style: add panda image to none existing path

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import "./MovieCard.css";
 import { img_300 } from "../../config/config";
 import { Badge } from "@material-ui/core";
+import Panda from '../../images/panda.jpg'
 
 const MovieCard = ({ id, title, poster, date, media, voteAverage }) => {
   return (
@@ -11,7 +12,7 @@ const MovieCard = ({ id, title, poster, date, media, voteAverage }) => {
       color={voteAverage > 6 ? "secondary" : "primary"}
     >
       <div className="media">
-        <img className="poster" src={`${img_300}/${poster}`} alt={title} />
+        <img className="poster" src={poster ? `${img_300}/${poster}` : Panda} alt={title} />
         <b className="title">{title}</b>
         <span>{media === "tv" ? "TV Series" : "Movie"}</span>
         <span className="date">{date}</span>
